1846dd0f9acd279955bb48295a799d82d87640c6
april1026 <36957874+april1026@users.noreply.github.com>
2018-06-19 23:27:18 +0800

    Update 動態記憶體配置(buggy).c

--- 動態記憶體配置(buggy).c
+++ 動態記憶體配置(buggy).c
@@ -6,16 +6,16 @@ int main(int argc, char *argv[]) {
 	int *ptr, n, i, search, num = -1;
 	printf ("請問要輸入幾個整數:");
 	scanf ("%d",  &n);
-	ptr = (int*) malloc (sizeof (int) *n );
+	ptr = (int*) malloc (sizeof (int) * n );
 	for (i = 0; i < n; i++) {
 		printf (" 第 %2d 個整數:", i + 1);
-		scanf ("%d", ptr + 1);
+		scanf ("%d", ptr + i);
 	}
 	printf ("\n");
 	printf ("請輸入要搜尋的值:");
 	scanf ("%d", &search);
 	for (i = 0; i < n; i++) {
-		if (* (ptr + 1) == search){
+		if (* (ptr + i) == search){
 			num = i;
 			break;
 		}
