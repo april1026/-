1ed49cebd43d7b81ad70a02f11447737f782ade3
april1026 <36957874+april1026@users.noreply.github.com>
2018-10-08 14:49:48 +0800

    Update 檔案讀寫.c

--- 檔案讀寫.c
+++ 檔案讀寫.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 #include <conio.h>
@@ -44,29 +42,5 @@ int main() {
 			break;
 		}
 	}
-	return 0;
-}  <io.h>
-#define LEN 100
-int main() {
-	FILE *fptr;
-	char fpath[LEN];
-	int fno, fsize;
-	
-	printf("輸入欲開啟的檔案路徑:");
-	gets(fpath);
-	
-	fptr = fopen(fpath, "r");
-	if (fptr == NULL) {
-		printf("開啟檔案失敗，%s 可能不存在\n", fpath);
-		exit(0);
-	}
-	fno = fileno(fptr);
-	fsize = filelength(fno);
-	printf("\n %s 開檔完成，檔案大小 %d Bytes\n", fpath, fsize);
-	char ch;
-	while((ch = fgetc(fptr)) != EOF) {
-		printf("%c", ch);
-	}
-	fclose(fptr);
 	return 0;
 } 
