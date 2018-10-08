189763304d20004457e0b1415728d8c52b8e1cbd
april1026 <36957874+april1026@users.noreply.github.com>
2018-10-08 14:07:05 +0800

    Create 讀檔.c

---
+++ 讀檔.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <io.h>
+#define LEN 100
+int main() {
+	FILE *fptr;
+	char fpath[LEN];
+	int fno, fsize;
+	
+	printf("輸入欲開啟的檔案路徑:");
+	gets(fpath);
+	
+	fptr = fopen(fpath, "r");
+	if (fptr == NULL) {
+		printf("開啟檔案失敗，%s 可能不存在\n", fpath);
+		exit(0);
+	}
+	fno = fileno(fptr);
+	fsize = filelength(fno);
+	printf("\n %s 開檔完成，檔案大小 %d Bytes\n", fpath, fsize);
+	fclose(fptr);
+	return 0;
+} 
